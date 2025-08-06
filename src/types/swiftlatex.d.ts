398d690ff7f537a968669b34ce45ddd174aa390c
Madalin Irimia <contact@irimiarecruitment.com>
2025-08-06 17:31:50 +0300

    ✅ Fix swiftlatex types by moving to src/types

--- src/types/swiftlatex.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'swiftlatex' {
-  export const PdfTeXEngine: any;
-  export const XeTeXEngine: any;
-  export const DvipdfmxEngine: any;
-
-  export interface LaTeXOpts {
-    [key: string]: any;
-  }
-}
